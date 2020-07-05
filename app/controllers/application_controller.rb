32187c1385df90fbf957f4c06f24c565840fcfa1
Toni  Diep <adiep123@gmail.com>
2020-07-05 14:50:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,6 +27,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
+      # @user = User.find_by_email(params[:email])
+      #   if @user && @user.authenticate(params[:password])
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
       session[:user_id] = @user.id
