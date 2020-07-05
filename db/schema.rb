947acbb6e4f05e108e091f5201b20604625f6a34
Toni  Diep <adiep123@gmail.com>
2020-07-05 03:16:37 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200704042414) do
+ActiveRecord::Schema.define(version: 20200705025318) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
